384de5468353abe66fafa777ed65053e8fc1be52
moznion <moznion@mail.moznion.net>
2026-05-28 15:18:34 +0900

    releng: v7.1.2
    Signed-off-by: moznion <moznion@mail.moznion.net>

--- src/index.ts
+++ src/index.ts
@@ -46,7 +46,7 @@ function wrap(reactHooksPlugin: ESLint.Plugin): ESLint.Plugin {
 const plugin = {
   meta: {
     name: "@moznion/eslint-plugin-selective-hooks",
-    version: "7.1.1",
+    version: "7.1.2",
   },
   rules,
   configs: {} as Record<string, unknown>,
